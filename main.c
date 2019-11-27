23e39ef2752d7dc66e3b171b952fdd7012408c02
Nitinkumar Verma <nverma@sensata.com>
2019-11-27 21:43:47 +0000

    added header file

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include "header.h"
 
+int x = 6;
 int main()
 {
     printf("Hello world\n");
+    printf("HELLO WORLD\n");
+    printf("%d",x);
     return 0;
 }
